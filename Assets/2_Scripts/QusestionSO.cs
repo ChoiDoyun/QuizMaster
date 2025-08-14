d9f79afdb100b6f82b8e7c92d7b359ccef9d7daa
ChoiDoyun <vahyeon2509@sonline20.sen.go.kr>
2025-08-14 11:51:50 +0900

    5 6 7 8

--- Assets/2_Scripts/QusestionSO.cs
+++ Assets/2_Scripts/QusestionSO.cs
@@ -5,4 +5,26 @@ public class QuestionSO : ScriptableObject
 {
     [TextArea(2, 6) ]
     [SerializeField] string question = "여기에 질문을 적어주세요.";
+    [SerializeField] string[] answers = new string[4];
+    [SerializeField] int correctAnswerIndex = 0;
+
+    public string GetQuestion()
+    {
+        return question;
+    }
+
+    public string GetAnswers(int index)
+    {
+        return answers[index];
+    }
+
+    public string GetCorrectAnswer()
+    {
+        return answers[correctAnswerIndex];
+    }
+
+    public int GetCorrectAnswerIndex()
+    {
+        return correctAnswerIndex;
+    }
 }
