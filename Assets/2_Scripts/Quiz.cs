96cd378c9db14a21868e63804156326d1089990f
ChoiDoyun <vahyeon2509@sonline20.sen.go.kr>
2025-08-26 11:46:59 +0900

    61

--- Assets/2_Scripts/Quiz.cs
+++ Assets/2_Scripts/Quiz.cs
@@ -1,3 +1,4 @@
+using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,27 +7,60 @@ public class Quiz : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI questionText;
     [SerializeField] QuestionSO question;
-    //[SerializeField] TextMeshProUGUI[] answerTextArr;
     [SerializeField] GameObject[] answerButtons;
     [SerializeField] Sprite defaultAnswerSprite;
     [SerializeField] Sprite correctAnswerSprite;
 
     void Start()
     {
+        GetNExtQuestion();
+    }
+
+    private void GetNExtQuestion()
+    {
+        SetButtonState(true);
+        SetDefaultButtonSprites();
+        OnDisplayQuestion();
+    }
+
+    private void SetDefaultButtonSprites()
+    {
+        foreach (GameObject obj in answerButtons)
+        {
+            obj.GetComponent<Image>().sprite = defaultAnswerSprite;
+        }
+    }
+
+    private void OnDisplayQuestion()
+    {
         questionText.text = question.GetQuestion();
 
-       for (int i = 0; i < answerButtons.Length; i++)
+        for (int i = 0; i < answerButtons.Length; i++)
         {
-            //answerTextArr[i].text = question.GetAnswers(i);
-            answerButtons[i].GetComponentInChildren<TextMeshProUGUI>().text = question.GetAnswers(i);
+            TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
         }
     }
+
     public void OnAnswerButtonClick(int index)
     {
+        answerButtons[index].GetComponent<Image>().sprite = correctAnswerSprite;
+
         if (index == question.GetCorrectAnswerIndex())
         {
-            questionText.text = "정답입니다!";
-            answerButtons[index].GetComponent<Image>().sprite = correctAnswerSprite;
+            questionText.text = "정답입니다!";    
+        }
+        else
+        {
+            questionText.text = "틀렸습니다. 정답은: " + question.GetCorrectAnswer();
+        }
+        SetButtonState(false);
+    }
+
+    private void SetButtonState(bool state)
+    {
+        foreach (GameObject obj in answerButtons)
+        {
+            obj.GetComponent<Button>().interactable = state;
         }
     }
-}   
+}
---
+++ Assets/2_Scripts/Timer.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] float problemTIme = 10f;
+    [SerializeField] float solutionTime = 3f;
+    float time = 0;
+
+    [HideInInspector] public bool isProblemTime = true;
+    [HideInInspector] public float fillAmount;
+
+    private void Start()
+    {
+        time = problemTIme;
+    }
+
+    private void Update()
+    {
+        TimerCountDown();
+        UpdateFillAmount();
+    }
+
+    private void UpdateFillAmount()
+    {
+        if (isProblemTime)
+        {
+            fillAmount = time / problemTIme;
+        }
+        else
+        {
+            fillAmount = time / solutionTime;
+        }
+    }
+
+    private void TimerCountDown()
+    {
+        Debug.Log("Time remaining: " + time);
+        time -= Time.deltaTime;
+        if (time <= 0)
+        {
+            if (isProblemTime)
+            {
+                isProblemTime = false;
+                time = solutionTime;
+            }
+            else
+            {
+                isProblemTime = true;
+                time = problemTIme;
+            }
+        }
+    }
+}
